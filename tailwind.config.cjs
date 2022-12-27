eb23e7d5d0ec7f64972cdcaef16343113eee563c
G808 <hucciallstars@gmail.com>
2022-12-27 22:58:23 +0200

    Project setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,26 @@ module.exports = {
         "secondary-400": "#FFCD58",
         "secondary-500": "#FFC132",
       },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "(url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
   plugins: [],
