2ebf345ce85d1a6a2fae84e8d45ec8dc2dfb5e8d
G808 <hucciallstars@gmail.com>
2022-12-28 00:59:36 +0200

    Responsive menu modal done

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -75,7 +75,41 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
       </div>
       {/* Mobile Menu modal */}
       {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 bottom-0 z-40 h-full w-[18.75rem]"></div>
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[18.75rem] bg-primary-100 drop-shadow-xl">
+          {/* Close Icon */}
+          <div className="flex justify-end p-12 ">
+            <button
+              onClick={() => {
+                setIsMenuToggled(!isMenuToggled);
+              }}
+            >
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          {/* Menu Items */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl ">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Plans"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        </div>
       )}
     </nav>
   );
